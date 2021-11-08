e7ebb2b289cbcdc2fe0c66747f939c811f005e3f
Mauricio <hedinyer.perucho@ustabuca.edu.co>
2021-11-08 08:23:56 -0600

    changes about the arguments
    i'll will put the arguments manually in yhe 
    python code and it at the same time

--- test.py
+++ test.py
@@ -8,7 +8,20 @@ This is a temporary script file.
 
 import subprocess
 
-p1 = subprocess.run(['D:/kraken/Space_GPS_spoofer/gpssim.exe'], capture_output=True,text=True)
-print(p1.args)
-print(p1.stdout)
-print(p1.stderr)
+# p1 = subprocess.run(['D:/kraken/Space_GPS_spoofer/gpssim.exe'], capture_output=True,text=True)
+# print(p1.args)
+# print(p1.stdout)
+# print(p1.stderr)
+
+
+
+from ctypes import *
+
+file = "D:/kraken/Space_GPS_spoofer/SDR_lib.so"
+fileRun = CDLL(file)
+
+result = fileRun.main()
+
+print(result)
+
+
